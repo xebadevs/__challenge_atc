efc252fc61cdd103f0a00ff71112c105f5d0345c
Sebastian Londero <xebadevs@gmail.com>
2022-04-03 23:59:33 -0300

    Search

--- src/components/createTeam/search/Search.jsx
+++ src/components/createTeam/search/Search.jsx
@@ -3,14 +3,13 @@ import SearchClubs from './SearchClubs'
 import SearchPlayers from './SearchPlayers'
 
 function Search() {
-    // * Modifica el contenido del Dropdown según la opción seleccionada
-    const [selectedDropdown, setSelectedDropdown] = useState('jugadores')
 
     return (
         <>
             <h1>Search Component</h1>
             <div>
-                <SearchPlayers player={'Haland'} />
+                {/* <SearchPlayers player={'Haland'} /> */}
+                <SearchPlayers />
                 {/* <SearchClubs /> */}
             </div>
         </>
--- src/components/createTeam/search/SearchPlayers.jsx
+++ src/components/createTeam/search/SearchPlayers.jsx
@@ -3,17 +3,18 @@ import axios from 'axios';
 import noPhoto from '../../../assets/img/nn_photo.png'
 
 
-function SearchPlayers( {player} ) {
-    const [players, setPlayers] = useState([])
+function SearchPlayers () {
     const API_KEY = '4fe77203ed7cebd9465165f180509282ca89a77f9b7d96c4b7b4abf878791181'
     const [info, setInfo] = useState(null)
+    const [renderInfo, setRenderInfo] = useState(false)
+    const [inputValue, setInputValue] = useState(null)
+    const [query, setQuery] = useState(null)
 
     useEffect(() => {
         let options = {
             method: 'GET',
-            url: 'https://apiv3.apifootball.com/?action=get_players&player_name=' + player + '&APIkey=' + API_KEY
+            url: 'https://apiv3.apifootball.com/?action=get_players&player_name=' + 'haland' + '&APIkey=' + API_KEY
         }
-
         axios
             .get(options.url, options)
             .then(res => {
@@ -25,14 +26,18 @@ function SearchPlayers( {player} ) {
             })
     }, [])
 
+    console.log('Info: ' + info)
+    console.log('Input: ' + inputValue)
+    console.log('Query: ' + query)
+
     return (
         <>
             <form>
-                <input className="form-control" type="search" placeholder="Buscar jugador..." aria-label="Search" onChange={e => setInfo(e.target.value)}/>
-                <button className="btn btn-warning" type="submit">Pelota</button>
+                <input className="form-control" type="search" placeholder="Buscar jugador..." aria-label="Search" onChange={e => setInputValue(e.target.value)} />
+                <button className="btn btn-warning" type="submit" onClick={() => setQuery(true)}>Pelota</button>
             </form>
 
-            {info &&
+            {query &&
                 <div>
                     {info.map((e) => (
                         <div key={e.player_id}>
