efc75b52fe97a05fc72ed81445689269f258381e
Sebastian Londero <xebadevs@gmail.com>
2022-04-04 11:55:56 -0300

    handle_api_errors

--- src/components/createTeam/search/SearchPlayers.jsx
+++ src/components/createTeam/search/SearchPlayers.jsx
@@ -2,49 +2,50 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import noPhoto from '../../../assets/img/nn_photo.png'
 import ShowPlayers from '../../../handlers/showPlayers/ShowPlayers';
+import { toBeEnabled } from '@testing-library/jest-dom/dist/matchers';
 
 
 function SearchPlayers ( {player} ) {
     const API_KEY = '4fe77203ed7cebd9465165f180509282ca89a77f9b7d96c4b7b4abf878791181'
     const [info, setInfo] = useState([])
-    const [renderInfo, setRenderInfo] = useState(false)
-    const [query, setQuery] = useState(false)
-    // const [player, setPlayer] = useState('haland')
     const [inputValue, setInputValue] = useState([])
-    let closting = 'haland'
+    const [renderInfo, setRenderInfo] = useState(false)
 
-    const getInfo = (param) => {
+    const getInfo = async (param) => {
         let options = {
             method: 'GET',
             url: 'https://apiv3.apifootball.com/?action=get_players&player_name=' + param + '&APIkey=' + API_KEY
         }
-        axios
+        await axios
             .get(options.url, options)
             .then(res => {
                 setInfo(res.data)
-                console.log(res.data)
-                // setRenderInfo(true)
+                console.log(res.data.error)
+                if(res.data.error === undefined){
+                    setRenderInfo(true)
+                }else{
+                    setRenderInfo(false)
+                }
             })
             .catch(err => {
-                console.log(err)
+                console.log('Error: ' + err)
             })
     }
-
-    // useEffect(() => {
-    //     getInfo()
-    // }, [])
-
-    // console.log('Player: ' + player)
-    console.log('InputValue: ' + inputValue)
 
     return (
         <>
             <div>
                 <input className="form-control" type="search" placeholder="Buscar jugador..." aria-label="Search" onChange={e => setInputValue(e.target.value)} />
-                <button className="btn btn-warning" onClick={() => getInfo(inputValue)}>Pelota</button>
+                {inputValue != [] &&
+                    <button className="btn btn-warning" disabled={false} onClick={() => getInfo(inputValue)}>Pelota</button>
+                }
             </div>
 
-            {inputValue != [] &&
+            {/* {inputValue != [] &&
+                <p>InputValue no es un array vacío</p>
+            } */}
+
+            {renderInfo &&
                 <div>
                     {info.map((e) => (
                         <div key={e.player_id}>
