0f9a07b16d8dc51c3ae8e7732723f48f34d73510
Sebastian Londero <xebadevs@gmail.com>
2022-04-04 20:40:03 -0300

    save_players

--- src/components/createTeam/search/SearchPlayers.jsx
+++ src/components/createTeam/search/SearchPlayers.jsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import noPhoto from '../../../assets/img/nn_photo.png'
-import ShowPlayers from '../../../handlers/showPlayers/ShowPlayers';
-import { toBeEnabled } from '@testing-library/jest-dom/dist/matchers';
-
+import Functions from '../../../handlers/functions/Functions'
 
 function SearchPlayers ( {player} ) {
     const API_KEY = '4fe77203ed7cebd9465165f180509282ca89a77f9b7d96c4b7b4abf878791181'
     const [info, setInfo] = useState([])
     const [inputValue, setInputValue] = useState([])
     const [renderInfo, setRenderInfo] = useState(false)
+    const [noResponse, setNoResponse] = useState(false)
+    let team1 = []
 
     const getInfo = async (param) => {
         let options = {
@@ -25,42 +25,68 @@ function SearchPlayers ( {player} ) {
                     setRenderInfo(true)
                 }else{
                     setRenderInfo(false)
+                    setNoResponse(true)
                 }
             })
             .catch(err => {
                 console.log('Error: ' + err)
             })
     }
+    
 
-    return (
-        <>
-            <div>
-                <input className="form-control" type="search" placeholder="Buscar jugador..." aria-label="Search" onChange={e => setInputValue(e.target.value)} />
-                {inputValue != [] &&
-                    <button className="btn btn-warning" disabled={false} onClick={() => getInfo(inputValue)}>Pelota</button>
-                }
-            </div>
+    function refresh(){
+        window.location.reload(true)
+    }
 
-            {/* {inputValue != [] &&
-                <p>InputValue no es un array vacío</p>
-            } */}
+    function addPlayer(id, position){
+        // console.log(id + ' ' + position)
+        // let items = localStorage.getItem('Team1')
+        // if(localStorage.getItem('Team')){
+        //     let itemsJSON = JSON.parse(items)
+        //     console.log(itemsJSON.id)
+            
+        //     if(itemsJSON.id === id){
+        //         alert('id repetido')
+        //     }
+        // }else{
+        //     localStorage.setItem('Team1', JSON.stringify({'id': id, 'position': position}))
+        // }
 
-            {renderInfo &&
-                <div>
-                    {info.map((e) => (
-                        <div key={e.player_id}>
-                            <img src={e.player_image? e.player_image : noPhoto}></img>
-                            <p>{e.player_name.toUpperCase()}</p>
-                            <p>{e.team_name}</p>
-                            <p hidden>{e.player_type}</p>
-                            <button>Seleccionar</button>
-                            <hr />
-                        </div>
-                    ))}
-                </div>
+        team1.push(["id", id, "position", position])
+        console.log(team1)
+    }
+
+return (
+    <>
+        <div>
+            <input className="form-control" type="search" placeholder="Buscar jugador..." aria-label="Search" onChange={e => setInputValue(e.target.value)} />
+            {inputValue != [] &&
+                <button className="btn btn-warning" disabled={false} onClick={() => getInfo(inputValue)}>Pelota</button>
             }
-        </>
-  )
+            <button className='btn btn-warning mx-3' onClick={refresh}>Refresh</button>
+        </div>
+
+        {renderInfo &&
+            <div>
+                {info.map((e) => (
+                    <div key={e.player_id}>
+                        <img src={e.player_image? e.player_image : noPhoto}></img>
+                        <p>{e.player_name.toUpperCase()}</p>
+                        <p>{e.team_name}</p>
+                        <p>{e.player_id}</p>
+                        <p hidden>{e.player_type}</p>
+                        <button onClick={() => addPlayer(e.player_id, e.player_type)}>Seleccionar</button>
+                        <hr />
+                    </div>
+                ))}
+            </div>
+        }
+
+        {noResponse &&
+            <p>¡Nadie con ese nombre por aquí!</p>
+        }
+    </>
+)
 }
 
 export default SearchPlayers
